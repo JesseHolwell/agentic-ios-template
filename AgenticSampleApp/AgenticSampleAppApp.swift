69146a9341d8eaad917f85293d2f38173a3fb7b2
Jesse Holwell <jholwell@capebionics.com>
2026-03-11 11:24:47 +1100

    set up sample project

---
+++ AgenticSampleApp/AgenticSampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AgenticSampleAppApp.swift
+//  AgenticSampleApp
+//
+//  Created by Jesse Holwell on 11/3/2026.
+//
+
+import SwiftUI
+
+@main
+struct AgenticSampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AgenticSampleAppTests/AgenticSampleAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  AgenticSampleAppTests.swift
+//  AgenticSampleAppTests
+//
+//  Created by Jesse Holwell on 11/3/2026.
+//
+
+import Testing
+@testable import AgenticSampleApp
+
+struct AgenticSampleAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
